fbdca8a8d6c666ce616910374a926f7ff835f294
Kim Yongwook <rladyddnrl@gmail.com>
2020-07-25 21:07:12 +0900

    added 2839 sol

--- 2839.py
+++ 2839.py
@@ -1,10 +1,18 @@
 def solver():
     N = int(input())
-    five_grams = N // 5
-    three_grams = (N - five_grams * 5) // 3
-    if N - five_grams * 5 - three_grams * 3 != 0:
+    remainder = N % 5
+    if N in [4, 7]:
         print("-1")
-    else:
-        print(five_grams + three_grams)
+    elif (remainder == 0): # 5
+        print(N // 5)
+    elif remainder == 1: # 6
+        print(N // 5 + 1)
+    elif remainder == 2: # 12(3*4)..
+        print(N // 5 + 2)
+    elif remainder == 3: # 3, 8...
+        print(N // 5 + 1)
+    else: # remainder == 4 
+        print(N // 5 + 2)
+
         
 solver()

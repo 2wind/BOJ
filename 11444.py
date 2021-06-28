4b691d5778411864009ff1733186d96723bc151f
Kim Yongwook <kim.yongwook.snu@gmail.com>
2021-06-28 20:15:56 +0900

    sol for 2749

--- 11444.py
+++ 11444.py
@@ -1,16 +1,38 @@
 magic_number = 1000000
+fisano = 15 * magic_number // 10
 
-# def fib(n):
+def fib(n):
+    matrix = [[1, 1], [1, 0]]
+    if n < 2:
+        return n
+
+    n = n % fisano
+
+    power(matrix, n-1)
+
+    return matrix[0][0] % magic_number
+
+def dot(M, N):
+    x = (M[0][0] * N[0][0]) + (M[0][1] * N[1][0])
+    y = (M[0][0] * N[0][1]) + (M[0][1] * N[1][1])
+    z = (M[1][0] * N[0][0]) + (M[1][1] * N[1][0])
+    w = (M[1][0] * N[0][1]) + (M[1][1] * N[1][1])
+
+    M[0][0], M[0][1], M[1][0], M[1][1] = x, y, z, w
+
+
+def power(matrix, n):
+    if n < 2:
+        return
+    M = [[1, 1], [1, 0]]
+
+    power(matrix, n // 2)
+    dot(matrix, matrix)
+
+    if (n%2 == 1):
+        dot(matrix, M)
+
+
+i = int(input())
+print(fib(i))
     
-#     fibo = [0, 1, 0]
-
-#     for i in range(n):
-#         fibo[2] = (fibo[0] % magic_number + fibo[1] % magic_number)
-#         fibo[0], fibo[1] = fibo[1], fibo[2]
-
-#     return fibo[1]
-
-
-# i = int(input())
-# print(fib(i-1))
-

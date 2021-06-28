55b00bd35fc8be16073b2eddc6374655514c4db4
Kim Yongwook <kim.yongwook.snu@gmail.com>
2021-06-28 20:27:56 +0900

    solution for 11444

--- 11444.py
+++ 11444.py
@@ -1,22 +1,20 @@
-magic_number = 1000000
-fisano = 15 * magic_number // 10
+magic_number = 1000000007
 
 def fib(n):
     matrix = [[1, 1], [1, 0]]
     if n < 2:
         return n
 
-    n = n % fisano
 
     power(matrix, n-1)
 
     return matrix[0][0] % magic_number
 
 def dot(M, N):
-    x = (M[0][0] * N[0][0]) + (M[0][1] * N[1][0])
-    y = (M[0][0] * N[0][1]) + (M[0][1] * N[1][1])
-    z = (M[1][0] * N[0][0]) + (M[1][1] * N[1][0])
-    w = (M[1][0] * N[0][1]) + (M[1][1] * N[1][1])
+    x = (M[0][0] * N[0][0] + M[0][1] * N[1][0]) % magic_number
+    y = (M[0][0] * N[0][1] + M[0][1] * N[1][1]) % magic_number
+    z = (M[1][0] * N[0][0] + M[1][1] * N[1][0]) % magic_number
+    w = (M[1][0] * N[0][1] + M[1][1] * N[1][1]) % magic_number
 
     M[0][0], M[0][1], M[1][0], M[1][1] = x, y, z, w
 

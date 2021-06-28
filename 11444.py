05cb3b8fdeb07ea37db05f2e5fbb06b00612d8d5
Kim Yongwook <kim.yongwook.snu@gmail.com>
2021-06-28 19:50:56 +0900

    log(n) sol for 11444 and 2749

---
+++ 11444.py
@@ -0,0 +1,16 @@
+magic_number = 1000000
+
+# def fib(n):
+    
+#     fibo = [0, 1, 0]
+
+#     for i in range(n):
+#         fibo[2] = (fibo[0] % magic_number + fibo[1] % magic_number)
+#         fibo[0], fibo[1] = fibo[1], fibo[2]
+
+#     return fibo[1]
+
+
+# i = int(input())
+# print(fib(i-1))
+

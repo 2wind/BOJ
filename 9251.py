063615af3da1bb727a07b9ff76e5d6c534d3784e
Kim Yongwook <kim.yongwook.snu@gmail.com>
2021-07-01 15:54:47 +0900

    solved 9251

---
+++ 9251.py
@@ -0,0 +1,13 @@
+A = input()
+B = input()
+
+LCS = [[0 for x in range(len(B)+1)] for y in range(len(A)+1)]
+
+for i in range(1,len(A)+1):
+    for j in range(1,len(B)+1):
+        if A[i-1] == B[j-1]:
+            LCS[i][j] = LCS[i-1][j-1] +1
+        else:
+            LCS[i][j] = max(LCS[i-1][j], LCS[i][j-1])
+
+print(LCS[-1][-1])

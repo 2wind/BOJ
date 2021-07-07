70d91928d02aa9a4b4ad570310634b75934d367a
Kim Yongwook <kim.yongwook.snu@gmail.com>
2021-07-07 13:13:26 +0900

    solved 2263 in iterative way

--- 2263.py
+++ 2263.py
@@ -3,9 +3,10 @@ from collections import deque
 n = int(input())
 inorder = input().split()
 postorder = input().split()
-sys.setrecursionlimit(10**9)
+
 # print(inorder)
 # print(postorder)
+
 
 class Node():
     def __init__(self, num):
@@ -16,7 +17,7 @@ class Node():
     def __str__(self):
         return str(self.num)
 
-def divide_tree(inl, postl, in0, in1, post0, post1):
+def divide_tree(inl, postl, in0, in1, post0, post1, parents):
     # print(" " +" "*in0*5 + "i0")
     # print(" " +" "*in1*5 + "i1")
     # print(inl)
@@ -24,36 +25,45 @@ def divide_tree(inl, postl, in0, in1, post0, post1):
     # print(" " +" "*post1*5 + "p1")
     # print(postl)
 
-    if post0+1 == post1:
-        # print(post1-1)
-        node = Node(postl[post1-1])
-        return node
-    elif post0 < post1:
-        # print(post1-1)
-        root = postl[post1-1]
-        i = inl.index(root, in0, in1+1)
-        # print(f"i = {i}, root = {root}")
-        node = Node(root)
-        # print(f"{inl[in0:i]} | {root} | {inl[i+1:in1]}")
-        # print(f"{postl[post0:post0+i-in0]} | {postl[post0+i-in0:post0+i-in0+(in1-(i+1))]} | {root}")
-        node.left =divide_tree(inl, postl, in0, i   ,post0          ,  post0 + i-in0 )
-        node.right=divide_tree(inl, postl, i+1, in1 , post0+i-in0   ,  post0+i-in0+(in1-(i+1)) )
-        return node
-    else:
-        return None
-    
-    
+    stack = deque()
+    stack.append((in0, in1, post0, post1, False))
 
-root = divide_tree(inorder, postorder, 0, len(inorder), 0, len(postorder))
-
-def preorder(node, q):
     
-    q.append(str(node))
-    if node.left:
-        preorder(node.left, q)
-    if node.right:
-        preorder(node.right, q)
+    while len(stack) > 0:
+        in0, in1, post0, post1, is_left = stack.pop()
 
-result = deque()
-preorder(root, result)
-print(" ".join(result))
+        if post0 < post1:
+            # print(post1-1)
+            root = postl[post1-1]
+            i = inl.index(root, in0, in1+1)
+            # print(f"i = {i}, root = {root}")
+            node = Node(root)
+            # print(f"{inl[in0:i]} | {root} | {inl[i+1:in1]}")
+            # print(f"{postl[post0:post0+i-in0]} | {postl[post0+i-in0:post0+i-in0+(in1-(i+1))]} | {root}")
+            if len(parents) > 0:
+                if is_left:
+                    parents[-1].left = node
+                else:
+                    parents[-1].right = node
+            stack.append((i+1, in1 , post0+i-in0   ,  post0-in0+in1-1, False))
+            stack.append((in0, i   ,post0          ,  post0 + i-in0 , True))
+            
+            parents.append(node)
+
+    
+parents = deque()
+divide_tree(inorder, postorder, 0, len(inorder), 0, len(postorder), parents)
+
+# def preorder(node, q):
+    
+#     q.append(str(node))
+#     if node.left:
+#         preorder(node.left, q)
+#     if node.right:
+#         preorder(node.right, q)
+
+# for i in parents:
+#     print(i)
+# result = deque()
+# preorder(parents[0], result)
+print(" ".join([str(x) for x in parents]))

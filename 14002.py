8805099aa5bc75396b5534771f32108d96839108
Kim Yongwook <kim.yongwook.snu@gmail.com>
2021-08-02 19:37:45 +0900

    solved 14003 by pair tracking

--- 14002.py
+++ 14002.py
@@ -5,25 +5,34 @@ import bisect
 N = int(input())
 A = list(map(int, sys.stdin.readline().strip().split()))
 
-LIS = [[A[0]]]
-LIS_last = [A[0]]
+# LIS = [[A[0]]]
+LIS_last = [sys.maxsize for i in range(N)]
+LIS_last[0] = A[0]
+LIS_length = 1
+track = []
 
-for i in range(1, N):
+for i in range(N):
     # print("LIS:", LIS[-1], "A[i]=", A[i])
     
-    if LIS[-1][-1] < A[i]:
-        # print("Append")
-        LIS.append(LIS[-1] + [A[i]])
-        LIS_last.append(A[i])
+    if LIS_last[LIS_length-1] < A[i]:
+        LIS_length += 1
+        LIS_last[LIS_length-1] = A[i]
+        track.append((A[i], LIS_length-1))
     else:
-        # print("Change")
         idx = bisect.bisect_left(LIS_last, A[i])
-        # print("idx:",idx)
         LIS_last[idx] = A[i] # insert at lower bound
-        replaced = LIS[idx-1] if idx > 0 else []
-        LIS[idx] = replaced + [A[i]] # insert at lower bound
+        track.append((A[i], idx))
     # print(LIS_last)
     # print(LIS)
+    # print(LIS_last, LIS_length)
 
-print(len(LIS[-1]))
-print(" ".join(map(str, LIS[-1])))
+
+index = LIS_length -1
+for i in range(len(track)-1, -1, -1):
+    pair = track[i]
+    if pair[1] == index:
+        LIS_last[index] = pair[0]
+        index -= 1
+
+print(LIS_length)
+print(" ".join(map(str, LIS_last[:LIS_length])))
